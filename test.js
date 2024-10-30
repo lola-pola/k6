998c389e7a28eac3a666935d62bb71181c1a28ba
Elhay Efrat <elhayef@monday.com>
2024-10-30 16:34:15 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export let options = {
+    vus: 10,         // Number of virtual users
+    duration: '30s', // Test duration
+};
+
+export default function () {
+    let response = http.get('https://www.google.com');
+
+    // Check to ensure the response status code is 200
+    check(response, {
+        'is status 200': (r) => r.status === 200,
+    });
+
+    // Sleep to simulate think time between requests
+    sleep(1);
+}
